8e02cce92ba1972cb34de737fea4a90de1fd301a
U-UCLAW\kjiang <kjiang@LAWA100TT.uclaw.uchicago.edu>
2015-03-06 12:04:40 -0600

    fixed headers for write_all_matches 03/06/15

--- pacer_lib/reader.py
+++ pacer_lib/reader.py
@@ -221,7 +221,8 @@ class docket_parser():
             if "detailed_info:" in r and '{' in r:
                 r = r.replace('detailed_info:\n','').replace("\"\"", "\"")
                 r = r.replace(" \"", " \\\"").replace("\" ","\\\" ") # fix internal quotation marks
-                r = r.replace(" \'", " \\\'").replace("\' ","\\\' ") # fix internal quotation marks
+                r = r.replace(" \'", " \\'").replace("\' ","\\' ") .replace("\'","\\\'") # fix internal quotation marks
+                r = r.replace("\\\',\\\'", " \',\'").replace("\\\':\\\'", " \':\'").replace("{\\\'", "{\'").replace("\\\'}", "\'}")
                 detailed_info = eval(r)
 
             # Legacy detailed_info
@@ -229,7 +230,8 @@ class docket_parser():
                 r = r.replace('detailed_info:', '')
                 r = r.replace('(','[').replace(')',']').replace("\"\"", "\"").strip()
                 r = r.replace(" \"", " \\\"").replace("\" ","\\\" ") # fix internal quotation marks
-                r = r.replace(" \'", " \\\'").replace("\' ","\\\' ") # fix internal quotation marks
+                r = r.replace(" \'", " \\\'").replace("\' ","\\\' ").replace("\'","\\\'") # fix internal quotation marks
+                r = r.replace("\\\',\\\'", " \',\'").replace("\\\':\\\'", " \':\'").replace("{\\\'", "{\'").replace("\\\'}", "\'}")
                 temp = eval(r)
                 detailed_info = {'searched_case_no':temp[0],
                                  'court_id':temp[1],
@@ -603,7 +605,7 @@ class docket_parser():
         indicate the source docket and are prefixed by **download_meta_** and
         **case_meta_**, respectively.
         """
-        csv_headers = ['date_filed', 'document_number', 'docket_description', 
+        csv_headers = ['docket_number', 'date_filed', 'document_number', 'docket_description', 
                        'link_exist', 'document_link', 'unique_id']
 
         # Check for all of the files that have been downloaded
@@ -774,7 +776,7 @@ class docket_processor():
             for num, row in enumerate(docket_reader):
                 # skip column headers
                 if not header_passed: 
-                    if row == ['date_filed', 'document_number', 'docket_description', 
+                    if row == ['docket_number', 'date_filed', 'document_number', 'docket_description', 
                                'link_exist', 'document_link', 'unique_id']:
                         header_passed = True
                         continue
@@ -852,7 +854,7 @@ class docket_processor():
 
         Returns nothing.
         """
-        csv_headers = ['date_filed', 'document_number', 'docket_description', 
+        csv_headers = ['docket_number', 'date_filed', 'document_number', 'docket_description', 
                        'link_exist', 'document_link', 'unique_id']
 
         suffix = suffix.replace('_', '').replace('/','').replace('\\','')
